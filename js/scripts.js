fe086dcc5c40ec31b9021992736abbb9d99a6314
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-10 18:50:50 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,36 @@ function getInputValues(){
     var year = document.getElementById("year").value;
     var month =parseInt(document.getElementById("month").value);
     var day = parseInt(document.getElementById("day").value);
-    gender =document.getElementById("gender").value;
-    if (month <= 0) {
+    gender =document.getElementsByName("gender").value;
+    if (day<=0){
+        alert("Enter valid day; between 1 and 31.");
+    }
+    else if (day>31){
+        alert("Enter valid day; between 1 and 31.");
+    }
+    else if (month <= 0) {
         alert("Enter valid month; between 1 and 12.");
     }
     else if (month>12){
         alert("Enter valid month; between 1 and 12.");
     }
-    else if (DD<=0){
-        alert("Enter valid day; between 1 and 31.");
-    }
-    else if (DD>31){
-        alert("Enter valid day; between 1 and 31.");
-    }
+
     else {
         DD = day;
         MM = month;
         var splitYear = year.split("");
         CC =parseInt(splitYear[0].concat(splitYear[1])) ;
         YY =parseInt(splitYear[2].concat(splitYear[3])) ;
-         d= parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
+        d= parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
+        
+        if (gender ="male"){
+            alert("Your Akan name is : "+ maleName[d]);
+       }
+       else if (gender = "female") {
+           alert ("Your Akan name is : "+ femaleName[d]);
+       }
     }
-   if (gender == "male"){
-        alert("Your Akan name is : "+ maleName[d]);
-   }
-   else {
-       alert ("Your Akan name is : "+ femaleName[d]);
-   }
+    
+   
+   
 }

73efe973fb641f057b9f13cd135fbf07b436d215
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-10 21:27:35 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function getInputValues(){
     var year = document.getElementById("year").value;
     var month =parseInt(document.getElementById("month").value);
     var day = parseInt(document.getElementById("day").value);
-    gender =document.getElementsByName("gender").value;
+    gender =document.getElementsByName("gender").id;
     if (day<=0){
         alert("Enter valid day; between 1 and 31.");
     }
@@ -29,12 +29,12 @@ function getInputValues(){
         YY =parseInt(splitYear[2].concat(splitYear[3])) ;
         d= parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
         
-        if (gender ="male"){
-            alert("Your Akan name is : "+ maleName[d]);
-       }
-       else if (gender = "female") {
-           alert ("Your Akan name is : "+ femaleName[d]);
-       }
+        if (gender = "female"){
+            alert("Your Akan name is : "+ femaleName[d]);
+        }
+        else if (gender = "male") {
+           alert ("Your Akan name is : "+ maleName[d]);
+        }
     }
     
    

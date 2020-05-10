2734d8a569619c2d1be06b29c5abee20ed93a8d0
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-10 11:54:39 +0300

    Update script.js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+var gender;
+var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var CC,YY,MM,DD;
+function getInputValues(){
+    var year = document.getElementById("year").value;
+    var month =parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    gender =document.getElementById("gender").value;
+    if (month <= 0) {
+        alert("Enter valid month; between 1 and 12.");
+    }
+    else if (month>12){
+        alert("Enter valid month; between 1 and 12.");
+    }
+    else if (DD<=0){
+        alert("Enter valid day; between 1 and 31.");
+    }
+    else if (DD>31){
+        alert("Enter valid day; between 1 and 31.");
+    }
+    else {
+        DD = day;
+        MM = month;("", 2)
+        var splitYear = year.split("")
+        CC =parseInt(splitYear[0].concat(splitYear[1])) ;
+        YY =parseInt(splitYear[2].concat(splitYear[3])) ;
+        var d= parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
+        alert(day[d]);
+
+
+    }
+   
+}

0a916f095e6ddc7f34cef07c9fa45ff6a022e33e
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-12 10:25:10 +0300

    Update whole site

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-var d ;
-var gender;
+
+var dayOfTheWeek=[ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var d ;
 var CC,YY,MM,DD;
+var gender;
+
 function getInputValues(){
     var year = document.getElementById("year").value;
     var month =parseInt(document.getElementById("month").value);
     var day = parseInt(document.getElementById("day").value);
-    gender =document.getElementsByName("gender").id;
     if (day<=0){
         alert("Enter valid day; between 1 and 31.");
     }
@@ -28,15 +30,19 @@ function getInputValues(){
         CC =parseInt(splitYear[0].concat(splitYear[1])) ;
         YY =parseInt(splitYear[2].concat(splitYear[3])) ;
         d= parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
-        
-        if (gender = "female"){
-            alert("Your Akan name is : "+ femaleName[d]);
-        }
-        else if (gender = "male") {
-           alert ("Your Akan name is : "+ maleName[d]);
+        gender = document.getElementsByName("gender");
+        for(var i=0;i<=gender.length;i++){
+        	if(gender[i].checked){
+            	if (gender[i].value === "male"){
+                    alert("Your Akan Name is : "+ maleName[d] );
+                }
+                else{
+                   alert( "Your gender is : "+ femaleName[d]);
+                }
         }
     }
     
+    
    
-   
+}
 }
